d55be33406e7e62ae6cb62cd1cbcb9e1805577b5
Abid Saleem <abidsaleem04@gmail.com>
2021-03-14 21:32:26 +0530

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,205 @@
+// creating an array and passing the number, questions, options, and answers
+
+let levels = [
+  {
+  question: "Select Your Level",
+  options: [
+    "Easy",
+    "Medium",
+    "Hard"
+  ]
+}
+]
+
+
+let questions = [
+
+// Level 1
+    {
+    numb: 1,
+    num :1,
+    question: "The computer's processor consists of the following parts",
+    answer: "Control Unit and ALU",
+    options: [
+      "CPU and Main Memory",
+      "Hard Disk and Floppy Drive",
+      "Main Memory and Storage",
+      "Control Unit and ALU"
+    ]
+  },
+    {
+    numb: 2,
+    num :2,
+    question: "If you restart your computer by selecting the restart button, it is known as ___",
+    answer: "Soft boot and Warm boot",
+    options: [
+      "Soft boot",
+      "Cold boot",
+      "Warm boot",
+      "Soft boot and Warm boot"
+    ]
+  },
+    {
+    numb: 3,
+    num :3,
+    question: "Which data structure uses LIFO?",
+    answer: "Stacks",
+    options: [
+      "Array",
+      "Int",
+      "Stacks",
+      "Queues"
+    ]
+  },
+    {
+    numb: 4,
+    num :4,
+    question: "One loop inside the body of another loop is called ",
+    answer: "Nested",
+    options: [
+      "Loop in loop",
+      "Nested",
+      "Double loops",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    num :5,
+    question: "What is the only thing that computers understand?",
+    answer: "Machine Code",
+    options: [
+      "Machine Code",
+      "Low Level Languages",
+      "High Level Languages",
+      "Algorithms"
+    ]
+  },
+
+
+// Level 3
+  {
+    numb: 6,
+    num :1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 7,
+    num :2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 8,
+    num :3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 9,
+    num :4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 10,
+    num :5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+
+  // Level 3
+  {
+    numb: 11,
+    num :1,
+    question: "int x=~1; What is the value of 'x'",
+    answer: "-2",
+    options: [
+      "1",
+      "-1",
+      "2",
+      "-2"
+    ]
+  },
+    {
+    numb: 12,
+    num :2,
+    question: "Following is the invalid inclusion of a file to the current program. Identify it.",
+    answer: "#include < file",
+    options: [
+      "#include <file>",
+      "#include “file”",
+      "#include < file",
+      "All of the above are invalid."
+    ]
+  },
+    {
+    numb: 13,
+    num :3,
+    question: "A local variable is stored in ___",
+    answer: "Stack segment",
+    options: [
+      "Code segment",
+      "Stack segment",
+      "Heap segment",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 14,
+    num :4,
+    question: "What is a generic class?",
+    answer: "Class template",
+    options: [
+      "Function template",
+      "Class template",
+      "Inherited class",
+      "None of the above."
+    ]
+  },
+    {
+    numb: 15,
+    num :5,
+    question: "Compiler generates ___ file",
+    answer: "Object code",
+    options: [
+      "Executable code",
+      "Object code",
+      "Assembly code",
+      "None of the above."
+    ]
+  },
+
+];
